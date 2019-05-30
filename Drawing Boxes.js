73f4df28a4431fc70038e794f130b95399dc1c27
olivervandusen <50715861+olivervandusen@users.noreply.github.com>
2019-05-30 10:49:06 -0700

    add drawing boxes

---
+++ Drawing Boxes.js
@@ -0,0 +1,44 @@
+var boxes = []
+let i = 0
+
+function setup() {
+  createCanvas(600, 400);
+  
+}
+
+function mouseDragged() {
+  let h = random(10, 50);
+  let w = random(10, 50);
+  boxes [i] = new Box(mouseX, mouseY, h, w);
+  i++
+}
+  
+function draw() {
+  background(0, 50);
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].move();
+    boxes[i].show();
+  }
+}
+
+class Box {
+ constructor(x, y, h, w) {
+   this.x = x;
+   this.y = y;
+   this.h = h;
+   this.w = w;
+ }
+  
+ move() {
+   this.x += random(-3, 3);
+   this.y += random(-3, 3);
+ }
+  
+ show() {   
+   stroke(200, 30, 150);
+   strokeWeight(2);
+   //fill(255, 10);
+   noFill();
+   rect(this.x, this.y, this.h, this.w);
+ }
+}
